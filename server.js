e2d9b7b592fd94f0aaf9cfd9c84f50361675cab1
kapilk34 <kapilkumargupta1511@gmail.com>
2026-06-02 22:12:17 +0530

    all done now...

--- server.js
+++ server.js
@@ -44,4 +44,7 @@ app.use('/api/bookings', bookingRouter)
 initializeSocketHandlers(io);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=> console.log(`Server running on the port ${PORT}`))
+
+server.listen(PORT,()=> {
+    console.log(`Server running on the port ${PORT}`)
+})
